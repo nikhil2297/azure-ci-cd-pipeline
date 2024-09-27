e3b7bd53778640cce6eee19d3743e85781af4d09
Shreyas Singh <singhshreyas906@gmail.com>
2024-09-27 14:44:26 -0400

    Health-check api added

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Nikhil Lohar this side. Welcome to my first Node.js app!');
 });
 
+app.get('/health-check', (req: Request, res: Response) => {
+    res.send('The server up and running!');
+  });
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
